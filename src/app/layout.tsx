ff490098af67b5cb7bba724c2f45c4e55aea4555
oyeniran Ayobami Paul <ap.oyeniran@gmail.com>
2024-09-04 06:03:53 +0100

    feat: :sparkles: directly used scripts for pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import Footer from "@/components/ui/footer";
 import NavBar from "@/components/ui/nav-bar";
 import { COMPANY_PROFILE } from "@/utils/const";
-import { GoogleTagManager } from "@next/third-parties/google";
 import { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { Toaster } from "sonner";
 import "./globals.css";
 
@@ -31,12 +31,35 @@ export default function RootLayout({
 }) {
      return (
           <html lang="en">
-               <GoogleTagManager gtmId="GTM-PRT4WHMD" />
+               {/* <GoogleTagManager gtmId="GTM-PRT4WHMD" /> */}
+
                <body className={`${inter.className}`}>
                     <NavBar />
                     {children}
                     <Footer />
                     <Toaster richColors />
+                    <Script id="facebook-pixel" strategy="afterInteractive">
+                         {`
+                              !function(f,b,e,v,n,t,s)
+                              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                              n.queue=[];t=b.createElement(e);t.async=!0;
+                              t.src=v;s=b.getElementsByTagName(e)[0];
+                              s.parentNode.insertBefore(t,s)}(window, document,'script',
+                              'https://connect.facebook.net/en_US/fbevents.js');
+                              fbq('init', '5596665852661106');
+                              fbq('track', 'PageView');
+                         `}
+                    </Script>
+                    <noscript>
+                         <img
+                              height="1"
+                              width="1"
+                              style={{ display: "none" }}
+                              src={`https://www.facebook.com/tr?id=5596665852661106&ev=PageView&noscript=1`}
+                         />
+                    </noscript>
                </body>
           </html>
      );
